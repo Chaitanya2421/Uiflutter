41277df751987972846d46af1996401c809f5578
Chaitanya2421 <chaitanyagangavarapu2005@gmail.com>
2025-06-04 11:05:05 +0530

    Create about_page.dart

---
+++ NavigationRouter/about_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'page_template.dart';
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageTemplate(
+      title: 'About',
+      description: 'I am a student of ace engineering college as a data science student i am  very good at acedmics.',
+    );
+  }
+}
