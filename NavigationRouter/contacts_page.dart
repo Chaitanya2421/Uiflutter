f01ef1025178ecb72614a1ccef511f8069749c87
Chaitanya2421 <chaitanyagangavarapu2005@gmail.com>
2025-06-04 11:08:21 +0530

    Create contacts_page.dart

---
+++ NavigationRouter/contacts_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'page_template.dart';
+
+class ContactPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageTemplate(
+      title: 'Contact',
+      description: 'Contacts :'
+          'mobile : 9346877119'
+          'email : aceec@gamil.com',
+
+    );
+  }
+}
